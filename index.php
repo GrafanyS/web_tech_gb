77850f2c492d51c4998f10d1eafaa5a0e23020b8
GrafanyS <admin@valet.of.by>
2023-02-25 15:31:20 +0300

    PHP+CSS+js+

--- index.php
+++ index.php
@@ -97,7 +97,8 @@ $workExperience = [
                         </div>
                     <?php endfor; ?>
                     <br>
-                    <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Языки</b>
+                    <p class="w3-large w3-text-theme">
+                        <b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Языки</b>
                     </p>
                     <p>Русский</p>
                     <div class="w3-light-grey w3-round-xlarge">
@@ -153,8 +154,8 @@ $workExperience = [
                 <?php endfor; ?>
             </div>
             <div class="w3-container w3-card w3-white">
-                <h2 class="w3-text-grey w3-padding-16"><i
-                            class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Образование</h2>
+                <h2 class="w3-text-grey w3-padding-16">
+                    <i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Образование</h2>
                 <div class="w3-container">
                     <h5 class="w3-opacity"><b>gb.ru</b></h5>
                     <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>at this moment</h6>
